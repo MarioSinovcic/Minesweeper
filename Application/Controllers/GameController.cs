35fc4ce45f1ff378631441259fe5b2c088cd13ea
Mario <mario.sinovcic@myob.com>
2021-04-13 10:34:22 +1200

    Created Controller
    - basic facade
    - needs more functionality around it

---
+++ Application/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+using Application.Behaviour.Setup;
+using Domain;
+using Domain.DTOs;
+using Domain.Entities;
+using Domain.Enums;
+
+namespace Application.Controllers
+{
+    public class GameController
+    {
+        //GridFactory
+
+        public GameStateDTO SetupGame()
+        {
+            var grid = new RandomGridSetup(10, 10, 5).CreateGrid();
+            return new GameStateDTO {Grid = grid, Coords = null, GameStatus = GameStatus.Playing};
+        }
+
+        public GameStateDTO HandleMove(Coords inputDto, GameStateDTO gameState)
+        {
+            return Minesweeper.PerformMove(new Move {Grid = gameState.Grid, Coords = inputDto});
+        }
+    }
+}
--- Application/Program.cs
+++ Application/Program.cs
@@ -1,8 +1,4 @@
-﻿using Application.Behaviour.Setup;
-using Domain;
-using Domain.DTOs;
-using Domain.Entities;
-using Domain.Enums;
+﻿using Application.Controllers;
 using Frontend;
 
 namespace Application
@@ -15,16 +11,15 @@ namespace Application
         {
             var outputHandler = new ConsoleOutputHandler();
             var inputHandler = new ConsoleInputHandler();
-            
-            var grid = new RandomGridSetup(10, 10, 5).CreateGrid();
-            var gameState = new GameStateDTO {Grid = grid, Coords = null, GameStatus = GameStatus.Playing};
-            //controller.setupGame() <- builder??
-            
+            var gameController = new GameController();
+
+            var gameState = gameController.SetupGame(); //<- builder??
+                
             outputHandler.DisplayGameState(gameState);
             while (true)
             {
-                var coords = inputHandler.GetTurnInput();
-                gameState = Minesweeper.PerformMove(new Move {Grid = grid, Coords = coords}); //TODO: setup mediating controller
+                var coords = inputHandler.GetTurnInput(); //should be inputDTO
+                gameState = gameController.HandleMove(coords, gameState); //TODO: setup mediating controller
                 outputHandler.DisplayGameState(gameState);
             }
         }
